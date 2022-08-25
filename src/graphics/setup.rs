a23829c846956260624c514830761bc5bf60c67a
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-08-26 00:20:30 +0200

    add shadows

--- src/graphics/setup.rs
+++ src/graphics/setup.rs
@@ -9,11 +9,22 @@ pub fn setup(
         transform: Transform::from_xyz(-5.0, 15.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+    let size = 10.0;
     commands.spawn_bundle(DirectionalLightBundle {
         transform: Transform::from_translation(Vec3::ZERO)
             .looking_at(Vec3::new(1.0, -2.0, 1.0), Vec3::Y),
         directional_light: DirectionalLight {
             illuminance: 32_000.0,
+            shadows_enabled: true,
+            shadow_projection: OrthographicProjection {
+                left: -size,
+                right: size,
+                bottom: -size,
+                top: size,
+                near: -size,
+                far: size,
+                ..default()
+            },
             ..default()
         },
         ..default()
