0cdd56ce41164b150a99823992c7bdd4022ca290
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-08-26 20:45:02 +0200

    increase shadow projection size

--- src/graphics/setup.rs
+++ src/graphics/setup.rs
@@ -9,7 +9,7 @@ pub fn setup(
         transform: Transform::from_xyz(-5.0, 15.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-    let size = 10.0;
+    let size = 11.0;
     commands.spawn_bundle(DirectionalLightBundle {
         transform: Transform::from_translation(Vec3::ZERO)
             .looking_at(Vec3::new(1.0, -2.0, 1.0), Vec3::Y),
